37f80dea1dcba8c6646a90e0c46d4ab2247b5c57
Michael Zuffoletti <mikezuff@gmail.com>
2023-11-22 06:02:02 -0800

    a second user can join the room

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+Credit: Web Dev Simplified: How To Create A Video Chat App With WebRTC [youtube](https://www.youtube.com/watch?v=DvlyzDZDEq4)
--- server.js
+++ server.js
@@ -2,7 +2,40 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
 
 const port = 3000
+
+app.set('view engine', 'ejs')
+// serve files from the public directory
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        console.log('joined room', roomId, userId)
+        
+        // https://socket.io/docs/v3/rooms/
+        socket.join(roomId)
+        // had to drop broadcast here because it was causing an error
+        //socket.to(roomId).broadcast.emit('user-connected', userId)
+        // TypeError: Cannot read properties of undefined (reading 'emit')
+        socket.to(roomId).emit('user-connected', userId)
+
+
+        //socket.on('disconnect', () => {
+            //console.log('disconnected')
+            //socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        //})
+    })
+})
+
 
 server.listen(port)
